2554a6940346d593000e3084e5695e123cfbf96c
Nyenskans <katarina.susic049@gmail.com>
2022-08-27 13:52:11 +0200

    added .gitignore file

--- src/test/java/class01/GitBasics.java
+++ src/test/java/class01/GitBasics.java
@@ -2,7 +2,7 @@ package class01;
 
 public class GitBasics {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        System.out.println("Hello World");
         // when we have completed a task up to a certain point or all the way, that is when we should commit
         // It is not recommended to have too many commits and to commit too frequently when the changes are very small
 
