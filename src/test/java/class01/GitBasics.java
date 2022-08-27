986fc70e3749a4fb555b15228c21fe5e571e1266
Nyenskans <katarina.susic049@gmail.com>
2022-08-27 12:34:46 +0200

    1st commit

---
+++ src/test/java/class01/GitBasics.java
@@ -0,0 +1,10 @@
+package class01;
+
+public class GitBasics {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        // when we have completed a task up to a certain point or all the way, that is when we should commit
+        // It is not recommended to have too many commits and to commit too frequently when the changes are very small
+
+    }
+}
