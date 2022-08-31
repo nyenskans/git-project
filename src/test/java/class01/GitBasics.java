9d58c7cdd422b4b20d55fdf22d2e91693fb17250
Nyenskans <katarina.susic049@gmail.com>
2022-08-31 12:28:28 +0200

    second commit

--- src/test/java/class01/GitBasics.java
+++ src/test/java/class01/GitBasics.java
@@ -5,6 +5,11 @@ public class GitBasics {
         System.out.println("Hello World");
         // when we have completed a task up to a certain point or all the way, that is when we should commit
         // It is not recommended to have too many commits and to commit too frequently when the changes are very small
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
 
     }
 }
